968ae562b4be95b8667a3dc444a81cbee06b19b3
Peter Bailis <pbailis@cs.berkeley.edu>
2014-02-19 12:59:26 -0800

    compile errors

--- core/src/main/scala/edu/berkeley/velox/util/NonThreadedExecutionContext.scala
+++ core/src/main/scala/edu/berkeley/velox/util/NonThreadedExecutionContext.scala
@@ -4,5 +4,6 @@ import scala.concurrent.ExecutionContext
 import java.util.concurrent.Executors
 
 object NonThreadedExecutionContext {
-  implicit lazy val context: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(32))
+  implicit lazy val executor = Executors.newFixedThreadPool(32)
+  implicit lazy val context: ExecutionContext = ExecutionContext.fromExecutor(executor)
 }
