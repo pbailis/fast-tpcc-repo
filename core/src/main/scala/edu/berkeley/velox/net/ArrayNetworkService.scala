d930317a7db54d4dc9f9458aea383fc2d3229712
Peter Bailis <pbailis@cs.berkeley.edu>
2014-02-19 13:02:10 -0800

    print

--- core/src/main/scala/edu/berkeley/velox/net/ArrayNetworkService.scala
+++ core/src/main/scala/edu/berkeley/velox/net/ArrayNetworkService.scala
@@ -304,7 +304,7 @@ class ArrayNetworkService(
   val tcpNoDelay: Boolean = true,
   val serverID: Integer = -1) extends NetworkService with Logging {
 
-  val executor = NonThreadedExecutionContext.executor//Executors.newFixedThreadPool(16,new ArrayNetworkThreadFactory())
+  val executor = Executors.newFixedThreadPool(16,new ArrayNetworkThreadFactory())
   val connections = new ConcurrentHashMap[NetworkDestinationHandle, SocketBufferPool]
   val nextConnectionID = new AtomicInteger(0)
   private val connectionSemaphore = new Semaphore(0)
--- core/src/main/scala/edu/berkeley/velox/util/NonThreadedExecutionContext.scala
+++ core/src/main/scala/edu/berkeley/velox/util/NonThreadedExecutionContext.scala
@@ -4,6 +4,5 @@ import scala.concurrent.ExecutionContext
 import java.util.concurrent.Executors
 
 object NonThreadedExecutionContext {
-  implicit lazy val executor = Executors.newFixedThreadPool(32)
-  implicit lazy val context: ExecutionContext = ExecutionContext.fromExecutor(executor)
+  implicit lazy val context: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(16))
 }
