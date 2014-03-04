59eb43d5f62a42b72825997c16e4a0dd34a076ab
Peter Bailis <pbailis@cs.berkeley.edu>
2014-03-03 21:38:46 -0800

    debugging

--- core/src/main/scala/edu/berkeley/velox/net/ArrayNetworkService.scala
+++ core/src/main/scala/edu/berkeley/velox/net/ArrayNetworkService.scala
@@ -103,8 +103,12 @@ class SocketBuffer(
         // wrap the array and write it out
         while(buf.hasRemaining) {
           channel.write(buf)
+          logger.error(s"sent $buf, remaining: ${buf.hasRemaining}")
         }
         pool.lastSent = System.currentTimeMillis
+
+        logger.error(s"sent $buf!")
+
 
         // reset write position
         buf.clear
