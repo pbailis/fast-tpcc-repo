3c56c797b3c0edf421310c14e2f07241cb0af1b2
Peter Bailis <pbailis@cs.berkeley.edu>
2014-03-04 22:10:51 -0800

    print debug in txns

--- core/src/main/scala/edu/berkeley/velox/benchmark/datamodel/Transaction.scala
+++ core/src/main/scala/edu/berkeley/velox/benchmark/datamodel/Transaction.scala
@@ -24,8 +24,6 @@ import edu.berkeley.velox.benchmark.operation.DeferredIncrement
 
 
 class Transaction(val txId: Long, val partitioner: TPCCPartitioner, val storage: StorageEngine, val messageService: InternalRPCService) extends Logging {
-  logger.error(s"creating txn $txId")
-
   def table(tableName: Int): Table = {
     return new Table(tableName, this)
   }
