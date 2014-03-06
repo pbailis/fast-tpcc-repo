0043203d5bc3ab47d7a968e16deb5c3a58d13938
Peter Bailis <pbailis@cs.berkeley.edu>
2014-03-05 22:22:47 -0800

    simplify server

--- core/src/main/scala/edu/berkeley/velox/server/VeloxServer.scala
+++ core/src/main/scala/edu/berkeley/velox/server/VeloxServer.scala
@@ -27,29 +27,10 @@ import edu.berkeley.velox.util.NonThreadedExecutionContext.context
 // keys to it.
 
 class VeloxServer extends Logging {
-  val storageEngine = new StorageEngine
-  storageEngine.initialize
-  val partitioner = new TPCCPartitioner
-  val lockManager = new LockManager
-
-  val internalServer = new InternalRPCService
-  val executor = internalServer.networkService.setExecutor()
-  internalServer.initialize()
-
-
-  internalServer.registerHandler(new InternalGetAllHandler)
-  internalServer.registerHandler(new InternalPreparePutAllHandler)
-  internalServer.registerHandler(new InternalCommitPutAllHandler)
-  internalServer.registerHandler(new InternalSerializableGetAllRequestHandler)
-  internalServer.registerHandler(new InternalSerializablePutAllRequestHandler)
-  internalServer.registerHandler(new InternalSerializableUnlockRequestHandler)
-
   // create the message service first, register handlers, then start the network
   val frontendServer = new FrontendRPCService
-  frontendServer.networkService.setExecutor(executor)
+  frontendServer.networkService.setExecutor()
 
-  frontendServer.registerHandler(new TPCCLoadRequestHandler)
-  frontendServer.registerHandler(new TPCCNewOrderRequestHandler)
   frontendServer.registerHandler(new SequenceNumberHandler)
 
   frontendServer.initialize()
@@ -58,106 +39,6 @@ class VeloxServer extends Logging {
    * Handlers for front-end requests.
    */
 
-  class TPCCLoadRequestHandler extends MessageHandler[TPCCLoadResponse, TPCCLoadRequest] {
-    def receive(src: NetworkDestinationHandle, msg: TPCCLoadRequest): Future[TPCCLoadResponse] = {
-      logger.info(s"Loading TPCC warehouse ${msg.W_ID}")
-      future { new TPCCLoadResponse }
-    }
-  }
-
-  class TPCCNewOrderRequestHandler extends MessageHandler[TPCCNewOrderResponse, TPCCNewOrderRequest] {
-    def receive(src: NetworkDestinationHandle, msg: TPCCNewOrderRequest): Future[TPCCNewOrderResponse] = {
-      future { new TPCCNewOrderResponse(false) }
-    }
-  }
-
-  /*
-   * Handlers for internal routed requests
-   */
-
-  // define handlers
-  class InternalPreparePutAllHandler extends MessageHandler[PreparePutAllResponse, PreparePutAllRequest] {
-    def receive(src: NetworkDestinationHandle, msg: PreparePutAllRequest): Future[PreparePutAllResponse] = {
-      future {
-        storageEngine.putPending(msg.values)
-        new PreparePutAllResponse
-      }
-    }
-  }
-
-  class InternalCommitPutAllHandler extends MessageHandler[CommitPutAllResponse, CommitPutAllRequest] {
-    def receive(src: NetworkDestinationHandle, msg: CommitPutAllRequest): Future[CommitPutAllResponse] = {
-      future {
-        storageEngine.putGood(msg.timestamp, msg.deferredIncrement)
-        new CommitPutAllResponse
-      }
-    }
-  }
-
-  class InternalGetAllHandler extends MessageHandler[GetAllResponse, GetAllRequest] {
-    def receive(src: NetworkDestinationHandle, msg: GetAllRequest): Future[GetAllResponse] = {
-      // returns true if there was an old value
-      future { new GetAllResponse(storageEngine.getAll(msg.keys, msg.values)) }
-    }
-  }
-
-  class InternalSerializableGetAllRequestHandler extends MessageHandler[SerializableGetAllResponse, SerializableGetAllRequest] {
-    def receive(src: NetworkDestinationHandle, msg: SerializableGetAllRequest) = {
-      future {
-        val ret = new util.HashMap[PrimaryKey, Row]
-        val keys = new util.ArrayList[PrimaryKey](msg.keys.keySet())
-        Collections.sort(keys)
-        val get_it = keys.iterator()
-        while(get_it.hasNext) {
-          val toGet = get_it.next()
-          val toGetRow = msg.keys.get(toGet).asInstanceOf[SerializableRow]
-          if(toGetRow.forUpdate) {
-            lockManager.writeLock(toGet, 8008)
-          } else {
-            lockManager.readLock(toGet, 8008)
-          }
-
-          ret.put(toGet, storageEngine.get(toGet))
-        }
-
-        new SerializableGetAllResponse(ret)
-      }
-    }
-  }
-
-  class InternalSerializablePutAllRequestHandler extends MessageHandler[SerializablePutAllResponse, SerializablePutAllRequest] {
-     def receive(src: NetworkDestinationHandle, msg: SerializablePutAllRequest) = {
-       future {
-         val keys = new util.ArrayList[PrimaryKey](msg.values.keySet())
-         Collections.sort(keys)
-
-         val put_it = keys.iterator()
-         while(put_it.hasNext) {
-           val toPut = put_it.next()
-           val toPutRow = msg.values.get(toPut).asInstanceOf[SerializableRow]
-           if(toPutRow.needsLock) {
-             lockManager.writeLock(toPut, 8008)
-           }
-
-           storageEngine.put(toPut, toPutRow)
-         }
-
-         new SerializablePutAllResponse
-       }
-     }
-   }
-
-  class InternalSerializableUnlockRequestHandler extends MessageHandler[Unit, SerializableUnlockRequest] {
-      def receive(src: NetworkDestinationHandle, msg: SerializableUnlockRequest) = {
-        future {
-          val key_it = msg.keys.iterator()
-          while(key_it.hasNext) {
-            val key = key_it.next()
-            lockManager.unlock(key)
-          }
-        }
-      }
-    }
 
   class SequenceNumberHandler extends MessageHandler[Long, SequenceNumberReq] {
     def receive(src: NetworkDestinationHandle, msg: SequenceNumberReq): Future[Long] = {
@@ -173,7 +54,7 @@ object VeloxServer extends Logging {
     logger.info("Initializing Server")
     VeloxConfig.initialize(args)
     // initialize network service and message service
-    val kvserver = new VeloxServer
+    new VeloxServer
   }
 }
 
